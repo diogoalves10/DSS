f6ffffa922c3a633ef5f0fc0045523482c634475
Diogo Alves <diogo.ar1997@sapo.pt>
2019-12-06 15:05:12 +0000

    Classes criadas

---
+++ Fase3/src/Album.java
@@ -0,0 +1,7 @@
+import java.util.List;
+import java.util.Map;
+
+public class Album {
+    private String nome;
+    private Map<String,Musica> musicas;
+}
---
+++ Fase3/src/Utilizador.java
@@ -0,0 +1,30 @@
+public class Utilizador {
+    private String nome;
+    private String email;
+    private String password;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
